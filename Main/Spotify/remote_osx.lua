d7f34c1805629ef934e3ae1ffd26d195bb63a7c6
Fredrik Ehnbom <fredrik@ehnbom.nu>
2014-02-03 16:47:05 +0100

    spotify/remote_osx: Fix issues

--- Main/Spotify/remote_osx.lua
+++ Main/Spotify/remote_osx.lua
@@ -37,12 +37,12 @@ actions.update = function ()
 		_title = artist .. " - " .. name;
 		_playing = true;
 	end
-	
+
 	if (_title ~= title) then
 		title = _title;
 		server.update({ id = "info", text = title });
 	end
-	
+
 	if (_playing ~= playing) then
 		playing = _playing;
 		if (playing) then
@@ -61,19 +61,19 @@ end
 --@help Start playback
 actions.play = function()
 	os.script("tell application \"Spotify\" to play");
-	update();
+	actions.update();
 end
 
 --@help Pause playback
 actions.pause = function()
 	os.script("tell application \"Spotify\" to pause");
-	update();
+	actions.update();
 end
 
 --@help Toggle playback state
 actions.play_pause = function()
 	os.script("tell application \"Spotify\" to playpause");
-	update();
+	actions.update();
 end
 
 --@help Lower volume
@@ -85,34 +85,36 @@ end
 actions.volume_up = function()
 	local curr = os.script("tell application \"Spotify\" to set out to sound volume");
 	curr = curr + 10;
-	os.script("tell application \"Spotify\" to set sound volume to (sound volume - 10)");
+	os.script("tell application \"Spotify\" to set sound volume to (sound volume + 10)");
 end
 
 --@help Mute volume
 actions.volume_mute = function()
-	local curr = os.script("tell application \"Spotify\" to set out to sound volume");
+	-- + 0 forces a conversion from string to number, otherwise the == 0 test will fail as "0" != 0
+	local curr = os.script("tell application \"Spotify\" to set out to sound volume") + 0;
 	if (curr == 0) then
 		os.script("tell application \"Spotify\" to set sound volume to " .. volume);
 	else
 		volume = curr;
+		os.script("tell application \"Spotify\" to set sound volume to " .. 0);
 	end
 end
 
 --@help Next track
 actions.next = function()
 	os.script("tell application \"Spotify\" to next track");
-	update();
+	actions.update();
 end
 
 --@help Previous track
 actions.previous = function()
 	os.script("tell application \"Spotify\" to previous track");
-	update();
+	actions.update();
 end
 
 --@help Stop playback
 actions.stop = function()
 	os.script("tell application \"Spotify\" to pause");
-	update();
+	actions.update();
 end
 
