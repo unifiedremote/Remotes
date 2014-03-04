d8cd5a4d1ced62893d9acb7b91cd996ac0424b9c
Philip Bergqvist <philip@unifiedintents.com>
2014-03-04 21:05:42 +0100

    Fixed File Manager.

--- Main/File Manager/remote.lua
+++ Main/File Manager/remote.lua
@@ -1,6 +1,3 @@
-
--------------------------------------------------------------------
-
 local fs = libs.fs;
 local server = libs.server;
 local task = libs.task;
@@ -8,21 +5,24 @@ local l = libs.log;
 
 -------------------------------------------------------------------
 
-local fileevents = 
+local dialog_items =
 {
-	{type="item", text = "Details", id="d"}, 
-	{type="item",text="Open", id="o"},
-	{type="item",text="Copy", id="c"}, 
-	{type="item",text="Cut", id="m"}, 
-	{type="item",text="Delete", id="del"}
-};
+	{ type="item", text = "Details", id = "details" }, 
+	{ type="item", text="Open", id = "open" },
+	{ type="item", text="Open All", id = "open_all" },
+	{ type="item", text="Copy", id = "copy" }, 
+	{ type="item", text="Cut", id = "cut" }, 
+	{ type="item", text="Delete", id = "delete" }
+}
 
-local selectedItem;
-local moveItem;
+local paste_item = nil;
+local paste_mode = nil;
+
+local selected_item;
 local path;
+
 local items = {};
 local stack = {};
-local isMovingOrCopying = false;
 
 -------------------------------------------------------------------
 
@@ -30,181 +30,238 @@ events.focus = function()
 	stack = {};
 	path = "";
 	table.insert(stack, path);
-	updateWithPath();
+	update();
 end
 
 -------------------------------------------------------------------
 
-function updateWithPath ()
+function update ()
 	items = {};
 	if path == "" then
 		local root = fs.roots();
-		l.trace("root");
 		for t = 1, #root do
-			local child = {};
-			child.type = "item";
-			child.icon = "folder";
-			child.text = root[t];
-			child.fullpath = root[t];
-			child.isdir = true;
-			table.insert(items, child);
+			table.insert(items, {
+				type = "item",
+				icon = "folder",
+				text = root[t],
+				path = root[t],
+				isdir = true
+			});
 		end
 	else
-		l.trace(path);
 		local dirs = fs.dirs(path);
 		local files = fs.files(path);	
 		for t = 1, #dirs do
-			local child = {};
-			child.type = "item";
-			child.icon = "folder";
-			child.text = fs.fullname(dirs[t]);
-			child.fullpath = dirs[t];
-			child.isdir = true;
-			table.insert(items, child);
+			table.insert(items, {
+				type = "item",
+				icon = "folder",
+				text = fs.fullname(dirs[t]),
+				path = dirs[t],
+				isdir = true
+			});
 		end
 		for t = 1, #files do
-			local child = {};
-			child.type = "item";
-			child.icon = "file";
-			child.text = fs.fullname(files[t]);
-			print(files[t]);
-			child.fullpath = files[t];
-			child.isdir = false;
-			table.insert(items, child);
+			table.insert(items, {
+				type = "item",
+				icon = "file",
+				text = fs.fullname(files[t]),
+				path = files[t],
+				isdir = false
+			});
 		end
 	end
-	server.update({ id = "lst", children = items});
+	server.update({ id = "list", children = items});
 end
 
 -------------------------------------------------------------------
-
-actions.onItem = function ( index )
-	index = index + 1;
-	if items[index].isdir then
+-- Invoked when an item in the list is pressed.
+-------------------------------------------------------------------
+actions.item = function (i)
+	i = i + 1;
+	if items[i].isdir then
 		table.insert(stack, path);
-		path = items[index].fullpath;
-		updateWithPath();
+		path = items[i].path;
+		update();
 	else
-		task.open(items[index].fullpath);
+		actions.open(items[i].path);
 	end
 end
 
-actions.onHolditem = function ( index )
-	selectedItem = items[index+1];
-	server.update({id = "fileevent", type="dialog", onTap="event", children = fileevents});
+-------------------------------------------------------------------
+-- Invoked when an item in the list is long-pressed.
+-------------------------------------------------------------------
+actions.hold = function (i)
+	selected_item = items[i+1];
+	server.update({ type="dialog", ontap="dialog", children = dialog_items });
 end
 
-actions.event = function ( index )
-	index = index +1;
-	local s = fileevents[index];
-	if s.id == "d" then
-		local details = "";
-		if fs.isdir(selectedItem.fullpath) then
-			details = " Name: " .. fs.fullname(selectedItem.fullpath) .. 
-			"\n Location: " .. fs.parent(selectedItem.fullpath) ..
-			"\n Files: " .. #fs.files(selectedItem.fullpath) ..
-			"\n Folders: " .. #fs.dirs(selectedItem.fullpath) ..
-			"\n Created: " .. os.date("%c", fs.created(selectedItem.fullpath));
-		else
-			details = " Name: " .. fs.fullname(selectedItem.fullpath) .. 
-			"\n Location: " .. fs.parent(selectedItem.fullpath) ..
-			"\n Size: " .. fs.size(selectedItem.fullpath) ..
-			"\n Created: " .. os.date("%c", fs.created(selectedItem.fullpath)) ..
-			"\n Modified: " .. os.date("%c", fs.modified(selectedItem.fullpath));
-		end
-		server.update({id="details", type="dialog", text=details, children={{type="button", text="OK", Sign=1}}});
-	elseif s.id == "m" then
-		server.update({id="Move", type="dialog", text="Navigate to folder and long press on folder name and select paste.", children={{type="button", text="OK", Sign=1}}});
+-------------------------------------------------------------------
+-- Invoked when a dialog item is selected.
+-------------------------------------------------------------------
 
-		if isMovingOrCopying then
-			table.remove(fileevents);
-		end		
-		table.insert(fileevents, {type="item",text="Paste", id="pmove"});
-		isMovingOrCopying = true;
-		moveItem = selectedItem;
-	elseif s.id == "pmove" then
-		if fs.isdir(selectedItem.fullpath) then
-			l.trace(moveItem.fullpath);
-			l.trace(fs.combine(selectedItem.fullpath, fs.fullname(moveItem.fullpath)));
-			fs.move(moveItem.fullpath, fs.combine(selectedItem.fullpath ,fs.fullname(moveItem.fullpath)));
+function show_dir_details (path)
+	local details = 
+		" Name: " .. fs.fullname(path) .. 
+		"\n Location: " .. fs.parent(path) ..
+		"\n Files: " .. #fs.files(path) ..
+		"\n Folders: " .. #fs.dirs(path) ..
+		"\n Created: " .. os.date("%c", fs.created(path));
+	server.update({ type = "dialog", text = details, children = {{type="button", text="OK"}} });
+end
+
+function show_file_details (path)
+	local details =
+		" Name: " .. fs.fullname(path) .. 
+		"\n Location: " .. fs.parent(path) ..
+		"\n Size: " .. fs.size(path) ..
+		"\n Created: " .. os.date("%c", fs.created(path)) ..
+		"\n Modified: " .. os.date("%c", fs.modified(path));
+	server.update({ type = "dialog", text = details, children = {{type="button", text="OK"}} });
+end
+
+actions.dialog = function (i)
+	i = i + 1;
+	local action = dialog_items[i].id;
+	local path = selected_item.path;
+	
+	if action == "details" then
+		
+		-- Show details for file or folder
+		if (fs.isdir(path)) then
+			show_dir_details(path);
 		else
-			fs.move(moveItem.fullpath, fs.parent(selectedItem.fullpath) .. fs.fullname(moveItem.fullpath));
+			show_file_details(path);
 		end
-		table.remove(fileevents);
-		updateWithPath();
-		isMovingOrCopying = false;
-	elseif s.id == "pcopy" then
-		if fs.isdir(selectedItem.fullpath) then
-			l.trace(moveItem.fullpath);
-			l.trace(fs.combine(selectedItem.fullpath, fs.fullname(moveItem.fullpath)));
-			fs.copy(moveItem.fullpath, fs.combine(selectedItem.fullpath ,fs.fullname(moveItem.fullpath)));
+		
+	elseif (action == "cut") then
+	
+		-- Explain how to move
+		server.update({
+			type = "message", 
+			text = "Long-press a folder to paste."
+		});
+		if (paste_mode == nil) then
+			table.insert(dialog_items, { type="item", text="Paste", id="paste"});
+		end
+		paste_item = selected_item
+		paste_mode = "move";
+		update();
+	
+	elseif (action == "copy") then
+	
+		-- Explain how to move
+		server.update({
+			type = "message", 
+			text = "Long-press a folder to paste."
+		});
+		if (paste_mode == nil) then
+			table.insert(dialog_items, { type="item", text="Paste", id="paste"});
+		end
+		paste_item = selected_item
+		paste_mode = "copy";
+		update();
+	
+	elseif (action == "paste") then
+		
+		-- Determine source and destination
+		local source = paste_item.path;
+		local destination = "";
+		if (fs.isdir(path)) then
+			destination = fs.combine(path, fs.fullname(paste_item.path));
 		else
-			fs.copy(moveItem.fullpath, fs.parent(selectedItem.fullpath) .. fs.fullname(moveItem.fullpath));
+			destination = fs.combine(fs.parent(path), fs.fullname(paste_item.path));
 		end
-		table.remove(fileevents);
-		isMovingOrCopying = false;
-		updateWithPath();
-	elseif s.id == "del" then
-		server.update({id="dele", type="dialog", text="Do you realy like to delete " .. selectedItem.fullpath .. "?", children={{type="button", text="YES", onTap="deleteItem"}, {type="button", text="NO"}}});	
-	elseif s.id == "c" then
-		server.update({id="Copy", type="dialog", text="Navigate to folder and long press on folder name and select paste.", children={{type="button", text="OK", Sign=1}}});
-		if isMovingOrCopying then
-			table.remove(fileevents);
+		
+		-- Perform paste depending on mode
+		if (paste_mode == "move") then
+			fs.move(source, destination);
+		elseif (paste_mode == "copy") then
+			fs.copy(source, destination);
 		end
-		table.insert(fileevents, {type="item",text="Paste", id="pcopy"});
-		moveItem = selectedItem;
-		isMovingOrCopying = true;
-	elseif s.id == "o" then
-		task.open(selectedItem.fullpath);
+		
+		-- Reset paste stuff
+		table.remove(dialog_items);
+		paste_item = nil;
+		paste_mode = nil;
+		update();
+		
+	elseif (action == "delete") then
+	
+		-- Prompt to delete
+		server.update({ 
+			type="dialog", text="Are you sure you want to delete " .. path .. "?", 
+			children = {
+				{ type="button", text="Yes", ontap="delete" }, 
+				{ type="button", text="No" }
+			}
+		});
+	
+	elseif (action == "open") then
+	
+		-- Open the file or folder
+		actions.open(path);
+	
+	elseif (action == "open_all") then
+	
+		-- Open all the files inside a folder
+		if (fs.isdir(path)) then
+			local cmd = "";
+			local files = fs.files(path);
+			for t = 1, #files do
+				cmd = cmd .. "\"" .. files[t] .. "\"; ";
+			end
+			actions.open(cmd);
+		end
+	
 	end
 
 end
 
-actions.deleteItem = function ()
-	fs.delete(selectedItem.fullpath, true);
-	updateWithPath();
+actions.delete = function ()
+	local path = selected_item.path;
+	fs.delete(path, true);
+	update();
 end
 
 actions.back = function ()
-		path = table.remove(stack);
-	    print(path);
-		updateWithPath();
-		if #stack == 0 then
-			table.insert(stack, "");
-		end
+	path = table.remove(stack);
+	update();
+	if #stack == 0 then
+		table.insert(stack, "");
+	end
 end
 
 actions.up = function ()
 	table.insert(stack, path);
 	path = fs.parent(stack[#stack]);
-	updateWithPath();
+	update();
 end
 
 actions.home = function ()
 	table.insert(stack, path);
 	path = "";
-	updateWithPath();
+	update();
 end
 
 actions.refresh = function ()
-	updateWithPath();
+	update();
 end
 
 actions.goto = function ()
-	server.update({id = "go", type="input", onTap="gotopath", text="Goto"});
+	server.update({id = "go", type="input", ontap="gotopath", text="Goto"});
 end
 
-actions.gotopath = function (path)
+actions.gotopath = function (p)
 	if fs.isfile(path) then
-		task.open(path);
+		actions.open(path);
 	else
-		path = path;
-		updateWithPath();
+		path = p;
+		update();
 	end
 end
 
---@help Open file or folder on computer
+--@help Open file or folder on computer.
 --@param path
-actions.open = function( path )
-	task.open(path);
+actions.open = function (path)
+	os.open(path);
 end
