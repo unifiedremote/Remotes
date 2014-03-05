92d93fee82ba1684888c0ec0771a5f65897af3d7
Philip Bergqvist <philip@unifiedintents.com>
2014-03-05 17:14:15 +0100

    Fixed Task Manager.

--- Pending/Task Manager/remote_win.lua
+++ Pending/Task Manager/remote_win.lua
@@ -24,16 +24,27 @@ actions.update = function (index)
 end
 
 actions.tap = function (index)
-	local item = list[index+1];
+	item = list[index+1];
 	win.switchto(item.handle);
 end
 
 actions.hold = function (index)
-	local item = list[index];
+	item = list[index+1];
 	local dialog = { type = "dialog", title = "Task", ontap="dialog", children = { 
 		{ type = "item", text = "Close" },
 		{ type = "item", text = "Quit" },
 		{ type = "item", text = "Kill" }
 	}};
 	server.update(dialog);
+end
+
+actions.dialog = function (index)
+	if index == 0 then
+		win.close(item.handle);
+	elseif index == 1 then
+		win.quit(item.handle);
+	elseif index == 2 then
+		win.kill(item.handle);
+	end
+	actions.update();
 end
