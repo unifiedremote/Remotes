e5ae6e37fe2670f5443ed51f0187f52dfee3f28d
Philip Bergqvist <philip@unifiedintents.com>
2014-03-13 18:16:08 +0100

    Added Monitor for Mac OS X

---
+++ Main/Monitor/remote_osx.lua
@@ -0,0 +1,17 @@
+keyboard = libs.keyboard;
+
+actions.turn_on = function ()
+
+end
+
+actions.turn_off = function ()
+	keyboard.stroke("ctrl", "shift", "eject");
+end
+
+actions.brightness_up = function ()
+	keyboard.press("brightnessup");
+end
+
+actions.brightness_down = function ()
+	keyboard.press("brightnessdown");
+end
