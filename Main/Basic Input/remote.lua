054ec01e104a56d395427954d934b304aacd6ec8
Philip Bergqvist <philip@unifiedintents.com>
2014-03-24 15:15:14 +0100

    Revert "Removed old Basic Input implementation."
    This reverts commit 78ccb0454fb90056259f798920bbf19aec134904.

--- Main/Basic Input/remote.lua
+++ Main/Basic Input/remote.lua
@@ -1,2 +1,52 @@
--- This remote is just a placeholder for Basic Input
--- The mouse remote uses actions defined in the Core Input remote
+
+dragging = false;
+
+local mouse = libs.mouse;
+
+function update (r)
+	--server.update({id = "touch", text = r });
+end
+
+actions.down = function ()
+	update("down");
+end
+
+actions.up = function ()
+	update("up");
+end
+
+actions.tap = function ()
+	update("tap");
+	if (dragging) then
+		dragging = false;
+		mouse.dragend();
+		mouse.up();
+	else
+		mouse.click("left");
+	end
+end
+
+actions.double = function ()
+	update("double");
+	mouse.double("left");
+end
+
+actions.hold = function ()
+	update("hold");
+	mouse.down();
+	mouse.dragbegin();
+	dragging = true;
+end
+
+actions.delta = function  (id, x, y)
+	update("delta: " .. x .. " " .. y);
+	mouse.moveraw(x, y);
+end
+
+actions.left = function ()
+	mouse.click("left");
+end
+
+actions.right = function ()
+	mouse.click("right");
+end
