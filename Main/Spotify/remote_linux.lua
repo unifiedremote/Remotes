48ae41f890556f8f1d9ded87c19837a002f407ad
Philip Bergqvist <philip@unifiedintents.com>
2014-05-19 17:17:29 +0200

    Spotify for Linux

--- Main/Spotify/remote_linux.lua
+++ Main/Spotify/remote_linux.lua
@@ -21,6 +21,8 @@ actions.update = function ()
 	local _title = "";
 	local _playing = true;
 	
+	_title = os.execute("dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'|egrep -A 2 \"artist\"|egrep -v \"artist\"|egrep -v \"array\"|cut -b 27-|cut -d '\"' -f 1|egrep -v ^$` \"-\" `dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'|egrep -A 1 \"title\"|egrep -v \"title\"|cut -b 44-|cut -d '\"' -f 1|egrep -v ^$");
+	
 	if (_title == "") then
 		_title = "[Not Playing]";
 		_playing = false;
