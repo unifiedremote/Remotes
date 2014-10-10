cfb382f7004ca067a8c70cc806887d81d63a9e87
Richard Simko <richard.simko@icloud.com>
2014-10-10 14:27:37 +0200

    Fixed detection not working on OSX

--- Main/XBMC Keyboard/remote.lua
+++ Main/XBMC Keyboard/remote.lua
@@ -4,7 +4,7 @@ events.detect = function ()
 	if OS_WINDOWS then
 		return libs.fs.exists("C:\\Program Files (x86)\\XBMC\\XBMC.exe") or libs.fs.exists("C:\\Program Files\\XBMC\\XBMC.exe");
 	elseif OS_OSX then
-		return libs.fs.exists("/Applications/Spotify.app");
+		return libs.fs.exists("/Applications/XBMC.app");
 	end
 end
 
