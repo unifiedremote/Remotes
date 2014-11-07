6b432ea8ee97154b0549e4092beefb039f071941
Philip Bergqvist <philip@unifiedintents.com>
2014-11-07 13:49:01 +0100

    Improved screen cursor.

--- Main/Screen/remote.lua
+++ Main/Screen/remote.lua
@@ -1,7 +1,10 @@
 local screen = libs.screen;
+local mouse = libs.mouse;
 
 actions.update = function (x, y, w, h, update)
 	local res = screen.capture(x, y, w, h, update);
+	local x,y = mouse.position();
+	
 	libs.server.update({
 		id = "back",
 		x = res.x,
@@ -9,5 +12,9 @@ actions.update = function (x, y, w, h, update)
 		w = res.w,
 		h = res.h,
 		image = res.image,
+	},{
+		id = "cursor",
+		x = x,
+		y = y
 	});
 end
