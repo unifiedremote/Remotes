b560cbd1d306c74675c8da7fb555522a4d2e395b
Philip Bergqvist <philip@unifiedintents.com>
2015-01-23 11:21:46 +0100

    Added SoundCloud remote.

---
+++ Main/SoundCloud/remote.lua
@@ -0,0 +1,51 @@
+local keyboard = libs.keyboard;
+
+--@help Lower system volume
+actions.volume_down = function()
+	keyboard.stroke("shift", "down");
+end
+
+--@help Mute system volume
+actions.volume_mute = function()
+	keyboard.stroke("m");
+end
+
+--@help Raise system volume
+actions.volume_up = function()
+	keyboard.stroke("shift", "up");
+end
+
+--@help Previous track
+actions.previous = function()
+	keyboard.stroke("shift", "left"); 
+end
+
+--@help Next track
+actions.next = function()
+	keyboard.stroke("shift", "right");
+end
+
+--@help Toggle play & pause
+actions.play_pause = function()
+	keyboard.stroke("space");
+end
+
+--@help Repeat playing song
+actions.repeat_song = function ()
+	keyboard.stroke("shift", "L");
+end
+
+--@help Seek Forward
+actions.seek_forward = function ()
+	keyboard.stroke("right");
+end
+
+--@help Seek Backward
+actions.seek_backward = function ()
+	keyboard.stroke("left");
+end
+
+--@help Like playing song
+actions.like_song = function ()
+	keyboard.stroke("L");
+end
