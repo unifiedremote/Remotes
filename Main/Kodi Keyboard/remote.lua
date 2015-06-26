889d0592020220a0d0bd4348833d9f22d24f5cac
Philip Bergqvist <philip@unifiedintents.com>
2015-06-26 13:28:01 +0200

    Automatic task switching

--- Main/Kodi Keyboard/remote.lua
+++ Main/Kodi Keyboard/remote.lua
@@ -1,4 +1,5 @@
 local keyboard = libs.keyboard;
+local win = libs.win;
 
 --@help Launch Kodi application
 actions.launch = function()
@@ -9,23 +10,36 @@ actions.launch = function()
 	end
 end
 
+--@help Focus Kodi application
+actions.switch = function()
+	if OS_WINDOWS then
+		local hwnd = win.window("Kodi.exe");
+		if (hwnd == 0) then actions.launch(); end
+		win.switchtowait("Kodi.exe");
+	end
+end
+
 --@help Start playback
 actions.play = function()
+	actions.switch();
 	keyboard.stroke("space");
 end
 
 --@help Pause playback
 actions.pause = function()
+	actions.switch();
 	keyboard.stroke("space");
 end
 
 --@help Toggle play/pause
 actions.play_pause = function()
+	actions.switch();
 	keyboard.stroke("space");
 end
 
 --@help Raise volume
 actions.volume_up = function()
+	actions.switch();
 	if (OS_WINDOWS) then
 		keyboard.stroke("F10");
 	else
@@ -35,6 +49,7 @@ end
 
 --@help Lower volume
 actions.volume_down = function()
+	actions.switch();
 	if (OS_WINDOWS) then
 		keyboard.stroke("F9");
 	else
@@ -44,6 +59,7 @@ end
 
 --@help Toggle mute volume
 actions.volume_mute = function()
+	actions.switch();
 	if (OS_WINDOWS) then
 		keyboard.stroke("F8");
 	else
@@ -53,56 +69,67 @@ end
 
 --@help Navigate up
 actions.up = function()
+	actions.switch();
 	keyboard.stroke("up");
 end
 
 --@help Navigate left
 actions.left = function()
+	actions.switch();
 	keyboard.stroke("left");
 end
 
 --@help Navigate down
 actions.down = function()
+	actions.switch();
 	keyboard.stroke("down");
 end
 
 --@help Navigate right
 actions.right = function()
+	actions.switch();
 	keyboard.stroke("right");
 end
 
 --@help Select current item
 actions.select = function()
+	actions.switch();
 	keyboard.stroke("return");
 end
 
 --@help Stop playback
 actions.stop = function()
+	actions.switch();
 	keyboard.stroke("X");
 end
 
 --@help Play previous item
 actions.previous = function()
+	actions.switch();
 	keyboard.stroke("oem_comma");
 end
 
 --@help Play next item
 actions.next = function()
+	actions.switch();
 	keyboard.stroke("oem_period");
 end
 
 --@help Rewind
 actions.rewind = function()
+	actions.switch();
 	keyboard.stroke("R");
 end
 
 --@help Fast forward
 actions.fast_forward = function()
+	actions.switch();
 	keyboard.stroke("F");
 end
 
 --@help Navigate back
 actions.back = function()
+	actions.switch();
 	keyboard.stroke("esc");
 end
 
--- Main/Plex Keyboard/remote.lua
+++ Main/Plex Keyboard/remote.lua
@@ -1,10 +1,20 @@
 local kb = libs.keyboard;
+local win = libs.win;
 
 events.detect = function ()
 	if OS_WINDOWS then
 		return libs.fs.exists("C:\\Program Files (x86)\\Plex Home Theater") or libs.fs.exists("C:\\Program Files (x86)\\Plex\\Plex Media Center")  or libs.fs.exists("C:\\Program Files\\Plex\\Plex Media Center") or libs.fs.exists("C:\\Program Files\\Plex Home Theater");
 	elseif OS_OSX then
 		return libs.fs.exists("/Applications/Plex Home Theater.app") or libs.fs.exists("/Applications/Plex.app");
+	end
+end
+
+--@help Focus Plex application
+actions.switch = function()
+	if OS_WINDOWS then
+		local hwnd = win.window("Plex Home Theater.exe");
+		if (hwnd == 0) then actions.launch(); end
+		win.switchtowait("Plex Home Theater.exe");
 	end
 end
 
@@ -25,85 +35,102 @@ end
 
 --@help Navigate left
 actions.left = function ()
+	actions.switch();
 	kb.press("left");
 end
 
 --@help Navigate down
 actions.down = function ()
+	actions.switch();
 	kb.press("down");
 end
 
 --@help Navigate right
 actions.right = function ()
+	actions.switch();
 	kb.press("right");
 end
 
 --@help Navigate up
 actions.up = function ()
+	actions.switch();
 	kb.press("up");
 end
 
 --@help Select
 actions.select = function ()
+	actions.switch();
 	kb.press("return");
 end
 
 --@help Navigate back
 actions.back = function ()
+	actions.switch();
 	kb.press("esc");
 end
 
 --@help Play pause
 actions.play_pause = function ()
+	actions.switch();
 	kb.press("space");
 end
 
 --@help Seek forward
 actions.forward = function ()
+	actions.switch();
 	kb.press("f");
 end
 
 --@help Seek rewind
 actions.rewind = function ()
+	actions.switch();
 	kb.press("r");
 end
 
 --@help Navigate home
 actions.home = function ()
+	actions.switch();
 	kb.press("tab");
 end
 
 --@help Stop playback
 actions.stop = function ()
+	actions.switch();
 	kb.press("x");
 end
 
 --@help Show menu
 actions.menu = function ()
+	actions.switch();
 	kb.press("m");
 end
 
 --@help Next item
 actions.next = function ()
+	actions.switch();
 	kb.press("right");
 end
 
 --@help Previous item
 actions.previous = function ()
+	actions.switch();
 	kb.press("left");
 end
 
 --@help Play current item
 actions.play_current = function ()
+	actions.switch();
 	kb.press("space");
 end
 
 --@help Show OSD
 actions.osd = function ()
+	actions.switch();
 	kb.press("o");
 end
 
 --@help Show info
 actions.info = function ()
+	actions.switch();
 	kb.press("i");
 end
