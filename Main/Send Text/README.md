eee6dab8a73ab6eb22d5d850090898ff5c9b44d2
Filip Lindqvist <buggfille@gmail.com>
2015-07-29 18:19:32 +0200

    Updated Send Text, added copy to clipboard functionality

--- Main/Send Text/README.md
+++ Main/Send Text/README.md
@@ -3,8 +3,12 @@ Easily type text in your device and then send it to your computer.
 
 ## Features
 *  Send text
+*  Copy to clipboard
 *  Press ``Enter``
 *  Press ``Backspace``
+
+## Screenshots
+<img src="screen.png" width="200" />
 
 ## Support
 Developed and maintained by **Unified Remote**  
--- Main/Send Text/remote.lua
+++ Main/Send Text/remote.lua
@@ -1,5 +1,6 @@
-
-local keyboard = libs.keyboard;
+local keyboard = require("keyboard");
+local script = require("script");
+local utf8 = require("utf8");
 
 local _text = "";
 
@@ -22,5 +23,21 @@ end
 --@help Backspace
 actions.back = function()
 	keyboard.press("back");
+end
+
+--@help Copy to clipboard
+actions.copy = function()
+	local code = utf8.replace(_text, "\"", "\\\"");
+	print("Copying " .. code .. " into clipboard");
+	if OS_WINDOWS then
+		script.shell("echo \"" .. code .. "\" | clip");
+	elseif OS_OSX then
+		script.apple("set the clipboard to \"" .. code .."\"");
+	elseif OS_LINUX then
+		-- Might not work on every distribution..
+		script.shell("echo \"" .. code .. "\" | xclip -selection c")
+	else 
+		print("WTF!?");
+	end
 end
 
