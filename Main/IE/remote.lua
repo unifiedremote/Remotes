e07efd9b0b86ba82573d69a6205c5519fe914103
Filip Lindqvist <buggfille@gmail.com>
2015-07-30 10:24:32 +0200

    Updated Internet Explorer

--- Main/IE/remote.lua
+++ Main/IE/remote.lua
@@ -35,13 +35,13 @@ end
 --@help Go to next tab
 actions.next_tab = function()
 	actions.switch();
-	keyboard.stroke("contol", "shift", "tab");
+	keyboard.stroke("contol", "tab");
 end
 
 --@help Go to previous tab
 actions.previous_tab = function()
 	actions.switch();
-	keyboard.stroke("control", "tab");
+	keyboard.stroke("control", "shift", "tab");
 end
 
 --@help Open new tab
