5edcd64636153821408b379a23b6a3b77e057a92
Philip Bergqvist <philip@unifiedintents.com>
2015-08-05 12:04:08 +0200

    YouTube for new html player

--- Main/YouTube/remote.lua
+++ Main/YouTube/remote.lua
@@ -1,5 +1,17 @@
 local keyboard = libs.keyboard;
 
+function toast ()
+	if (settings.toast == "") then
+		libs.server.update({
+			type = "dialog",
+			text = "Volume control requires that the player has focus. Click in the player window using the mouse remote once to focus the player.",
+			title = "Volume",
+			children = { { type = "button", text = "OK" } }
+		});
+		settings.toast = "true";
+	end
+end
+
 --@help Launch YouTube
 actions.launch = function()
 	os.open("http://www.youtube.com");
@@ -7,22 +19,24 @@ end
 
 --@help Lower volume
 actions.volume_down = function()
+	toast();
 	keyboard.stroke("down");
 end
 
 --@help Raise volume
 actions.volume_up = function()
+	toast();
 	keyboard.stroke("up");
 end
 
 --@help Rewind
 actions.rewind = function()
-	keyboard.stroke("left");
+	keyboard.stroke("J");
 end
 
 --@help Fast forward
 actions.fast_forward = function()
-	keyboard.stroke("right");
+	keyboard.stroke("L");
 end
 
 --@help Play previous item
@@ -35,7 +49,7 @@ actions.next = function()
 	keyboard.stroke("shift", "n");
 end
 
---@help Enter fullscreen
+--@help Toggle fullscreen
 actions.fullscreen = function()
 	keyboard.stroke("F");
 end
