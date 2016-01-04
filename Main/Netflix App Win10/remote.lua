05b11c9cd6023a9b3c52faa5ad4c05ff9cd5ae91
Philip Bergqvist <philip@unifiedintents.com>
2016-01-04 15:39:57 +0100

    Temp Netflix fix

--- Main/Netflix App Win10/remote.lua
+++ Main/Netflix App Win10/remote.lua
@@ -1,80 +1,25 @@
 local kb = libs.keyboard;
-local uia = libs.uia;
 local win = libs.win;
-local tmr = libs.timer;
 local server = libs.server;
-local popup = nil;
-local tid = -1;
 
-local last_title = nil;
-local last_sne = nil;
-local last_et = nil;
-local last_pos = -1;
-local changing_pos = false;
-
-events.focus = function()
-	last_title = nil;
-	last_sne = nil;
-	last_et = nil;
-	last_pos = -1;
-	changing_pos = false;
-	tid = tmr.timeout(update, 1000);
+--@help Focus Netflix app
+actions.switch = function()
+	local hwnd = win.find(nil, "Netflix");
+	if (hwnd == 0) then return; end
+	win.switchtowait(hwnd);
 end
 
-events.blur = function()
-	tmr.cancel(tid);
-end
-
-function get_root()
-	local desktop = uia.desktop();
-	local root = uia.find(desktop, "Netflix", "children");
-	return root;
-end
-
-function begin_popup()
-	local root = get_root();
-	local appbar = uia.find(root, "BottomAppBar", "subtree");
-	local off = uia.property(appbar, "isoffscreen");
-	if (off) then
-		local player = uia.find(root, "Video player page", "subtree");
-		uia.dodefaultaction(player);
-		appbar = uia.find(root, "TopAppBar", "subtree");
-	end
-	popup = uia.parent(appbar);
-end
-
-function get_time()
-	local root = get_root();
-	local vp = uia.find(root, "Video player page", "subtree");
-	local v = uia.find(vp, "Video", "subtree");
-	return uia.property(v, "rangevaluemaximum");
-end
-
-function get_pos()
-	local root = get_root();
-	local vp = uia.find(root, "Video player page", "subtree");
-	local v = uia.find(vp, "Video", "subtree");
-	return uia.property(v, "rangevaluevalue");
-end
-
+--@help Toggle play pause
 actions.play_pause = function ()	
-	begin_popup();
-	local pp = uia.find(popup, "PauseResume", "subtree");
-	uia.toggle(pp);
+	kb.press("space");
 end
 
 actions.rewind = function ()
-	begin_popup();
-	local tp = uia.find(popup, "TrickPlay", "subtree");
-	local curr = uia.property(tp, "rangevaluevalue");
-	uia.rangesetvalue(tp, curr - 60);
+	
 end
 
 actions.forward = function ()
-	begin_popup();
-	local tp = uia.find(popup, "TrickPlay", "subtree");
-	local curr = uia.property(tp, "rangevaluevalue");
-	uia.rangesetvalue(tp, curr + 60);
+	
 end
 
 actions.volumeup = function()
@@ -89,47 +34,3 @@ actions.volumemute = function()
 	kb.press("volumemute");
 end
 
-actions.pos_stop = function(pos)
-	local t = get_time();
-	local newtime = (pos/100) * t;
-	begin_popup();
-	local tp = uia.find(popup, "TrickPlay", "subtree");
-	local curr = uia.property(tp, "rangevaluevalue");
-	uia.rangesetvalue(tp, newtime);
-	changing_pos = false;
-end
-
-actions.pos_start = function(pos)
-	changing_pos = true;
-end
-
-function update()
-	local t = get_time();
-	local pos = get_pos();
-	
-	-- extract current pos
-	if (pos ~= last_pos and changing_pos == false) then
-		local prog = math.round(pos/t * 100);
-		server.update({ id = "pos", progress = prog }); 
-		last_pos = pos;
-	end
-	
-	-- extrat current info
-	local root = get_root();
-	local tp = uia.find(root, "TopAppBar", "subtree");
-	local title = uia.name(uia.child(tp, 1));
-	local sne = uia.name(uia.child(tp, 2));
-	local et = uia.name(uia.child(tp, 3));
-	if (title ~= last_title) then
-		last_title = title;
-		last_sne = sne;
-		last_et = et;
-		server.update(
-			{ id = "title", text = title },
-			{ id = "sne", text = sne },
-			{ id = "et", text = et }
-		);
-	end
-	
-	tmr.timeout(update, 1000);
-end
