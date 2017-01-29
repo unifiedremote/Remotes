ab18c7b5521533bae0b42aec4fc525cdec4a8de4
Philip Bergqvist <philip@unifiedintents.com>
2017-01-29 15:41:14 +0100

    Fixed groove and added switchto

--- Main/Groove/remote.lua
+++ Main/Groove/remote.lua
@@ -1,41 +1,55 @@
 local keyboard = libs.keyboard;
+local win = libs.win;
+local utf8 = libs.utf8;
+
+--@help Focus Chrome application
+actions.switch = function()
+	local hwnds = win.findall(0, "ApplicationFrameWindow", nil, false);
+	for i,hwnd in ipairs(hwnds) do
+		local title = win.title(hwnd);
+		if utf8.startswith(title, "Groove") then
+			win.switchtowait(hwnd);
+		end
+	end
+end
 
 --@help Lower system volume
 actions.volume_down = function()
-	keyboard.press("f8");
+	keyboard.stroke("f8");
 end
 
 --@help Mute system volume
 actions.volume_mute = function()
-	keyboard.press("f7");
+	keyboard.stroke("f7");
 end
 
 --@help Raise system volume
 actions.volume_up = function()
-	keyboard.press("f9");
+	keyboard.stroke("f9");
 end
 
 --@help Previous track
 actions.previous = function()
-	keyboard.press("ctrl", "b"); 
+	keyboard.stroke("ctrl", "b"); 
 end
 
 --@help Next track
 actions.next = function()
-	keyboard.press("ctrl", "f");
+	keyboard.stroke("ctrl", "f");
 end
 
 --@help Toggle playback state
 actions.play_pause = function()
-	keyboard.press("ctrl", "p");
+	actions.switch();
+	keyboard.stroke("ctrl", "p");
 end
 
 --@help Toggle repeat
 actions.toggle_repeat = function()
-	keyboard.press("ctrl", "t");
+	keyboard.stroke("ctrl", "t");
 end
 
 --@help Toggle shuffle
 actions.toggle_shuffle = function()
-	keyboard.press("ctrl", "h");
+	keyboard.stroke("ctrl", "h");
 end
